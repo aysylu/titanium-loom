ac7ae5bcf723b5c9e2d750877b51459966b591b1
Aysylu <aysylu7@gmail.com>
2013-09-04 17:36:11 -0400

    updated loom version to the newest

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aysylu/loom "0.3.1"]
+                 [aysylu/loom "0.4.0"]
                  [clojurewerkz/titanium "1.0.0-beta1"]])
