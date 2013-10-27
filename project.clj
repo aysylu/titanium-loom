c8212c8e8ce2aea038650834b91c0b3efbb4a413
Aysylu <aysylu7@gmail.com>
2013-10-26 21:53:36 -0400

    added alias for leiningen, prepared for release

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject aysylu/titanium-loom "0.1.3-SNAPSHOT"
+(defproject aysylu/titanium-loom "0.1.3"
   :description "Titanium-Loom integration library"
   :url "https://github.com/aysylu/titanium-loom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [aysylu/loom "0.4.0"]
-                 [clojurewerkz/titanium "1.0.0-beta1"]])
+                 [aysylu/loom "0.4.1"]
+                 [clojurewerkz/titanium "1.0.0-beta1"]]
+  :aliases {"release" ["do" "clean," "with-profile" "default" "deploy" "clojars"]})
